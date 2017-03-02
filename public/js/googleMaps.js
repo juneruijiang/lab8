b1e780d848d18e6e4c7be51d32d6172adf177bdf
Rui Jiang <juneruijiang@gmail.com>
2017-03-02 03:34:28 -0800

    thirdpartyAPI lab

---
+++ public/js/googleMaps.js
@@ -0,0 +1,19 @@
+/**
+ * Created by juneruijiang on 3/2/17.
+ */
+function initMap() {
+    // Create center marker at UCSD
+    var ucsd_ltlng = {lat:32.885002149576245, lng:-117.24129527807236};
+
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: ucsd_ltlng,
+        zoom: 18
+    });
+
+    var marker = new google.maps.Marker({
+        position: ucsd_ltlng,
+        map: map,
+        title: 'UCSD'
+    });
+}
